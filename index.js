57c4298a4a7464ca5abba2b15b17041677f5d334
gustcomer <gustcomer@gmail.com>
2024-03-28 07:45:07 -0300

    feat: connect to mongoose

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose")
 
 const app = express();
+
+mongoose
+  .connect('mongodb://root:123456@mongo:27017/?authSource=admin')
+  .then(() => console.log("successfully connected to DB"))
+  .catch((e) => console.log(e))
 
 app.get("/", (req, res) => {
   res.send("<h2>Holla there!!!</h2>");
